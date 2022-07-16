737028d6effef5dd8b8ca1a683c662e6633823f3
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-07-16 01:06:37 -0700

    importexportlab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import {Username} from  "../data/user";
+import {city} from "../data/user"
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {Username} is a Web Developer from {city}
       </h1>
     </div>
   );
-}
+} 
+export default Home
